ca2a6acb33666d6d938d4f4d03f49d78185ec82e
Douglas Souza <douglas_souza03@hotmail.com>
2019-03-27 16:01:38 -0300

    service and controller 'course' class

--- src/main/java/br/com/ds/microservices/endpoint/services/CourseService.java
+++ src/main/java/br/com/ds/microservices/endpoint/services/CourseService.java
@@ -1,7 +1,25 @@
 package br.com.ds.microservices.endpoint.services;
 
+import br.com.ds.microservices.model.Course;
+import br.com.ds.microservices.repository.CourseRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+
 /**
  * @author Douglas Souza on 27/03/2019
  */
+@Service
+@Slf4j
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class CourseService {
+    private final CourseRepository courseRepository;
+
+    public Iterable<Course> list(Pageable pageable) {
+        log.info("Listing all courses");
+        return courseRepository.findAll(pageable);
+    }
 }
