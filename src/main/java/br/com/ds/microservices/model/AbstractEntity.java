880fb33d22ce98d4c80f780caa97682412fcf423
Douglas Souza <douglas_souza03@hotmail.com>
2019-03-27 16:00:16 -0300

    model class

--- src/main/java/br/com/ds/microservices/model/AbstractEntity.java
+++ src/main/java/br/com/ds/microservices/model/AbstractEntity.java
@@ -1,7 +1,10 @@
 package br.com.ds.microservices.model;
 
+import java.io.Serializable;
+
 /**
  * @author Douglas Souza on 27/03/2019
  */
-public interface AbstractEntity {
+public interface AbstractEntity extends Serializable {
+    Long getId();
 }
--- src/main/java/br/com/ds/microservices/model/Course.java
+++ src/main/java/br/com/ds/microservices/model/Course.java
@@ -1,7 +1,29 @@
 package br.com.ds.microservices.model;
 
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
 /**
  * @author Douglas Souza on 27/03/2019
  */
-public class Course {
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Course implements AbstractEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @NotNull(message = "The field 'title' is necessary")
+    @Column(nullable = false)
+    private String title;
+
 }
