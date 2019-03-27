cb86f6d849a6ba980b870cd67986e9753099c9d4
Douglas Souza <douglas_souza03@hotmail.com>
2019-03-27 16:00:50 -0300

    repository with paging

--- src/main/java/br/com/ds/microservices/repository/CourseRepository.java
+++ src/main/java/br/com/ds/microservices/repository/CourseRepository.java
@@ -1,7 +1,10 @@
 package br.com.ds.microservices.repository;
 
+import br.com.ds.microservices.model.Course;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
 /**
  * @author Douglas Souza on 27/03/2019
  */
-public interface CourseRepository {
+public interface CourseRepository extends PagingAndSortingRepository<Course, Long> {
 }
